0d4a3e86209ff885533bff56cd89fd39d9baa24f
stone <stone.grandia@163.com>
2024-08-10 12:06:33 +0800

    fix ci

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,11 @@ export default defineConfig({
         },
         rollupOptions: {
             // 确保外部化处理那些你不想打包进库的依赖
-            external: ['vue', 'vue-router', '@cogears/app-view', '@cogears/http-client'],
+            external: [
+                'vue', 'vue-router',
+                '@cogears/app-view', '@cogears/app-view/dist/style.css',
+                '@cogears/http-client',
+            ],
             output: {
                 // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
                 globals: {
